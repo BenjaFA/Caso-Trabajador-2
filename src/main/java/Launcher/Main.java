bc7544825192d45e77ac21657a1a44b1ef013318
Benjamin Fernandez <142475169+BenjaFA@users.noreply.github.com>
2023-11-23 19:32:51 -0300

    Codigo Fuente

---
+++ src/main/java/Launcher/Main.java
@@ -0,0 +1,9 @@
+package Launcher;
+
+import vista.FmrInterfazTrabajador;
+
+public class Main {
+    public static void main(String[] args) {
+        new FmrInterfazTrabajador();
+    }
+}
---
+++ src/main/java/datos/ArchivoDatos.java
@@ -0,0 +1,8 @@
+package datos;
+
+public interface ArchivoDatos {
+    public void leer();
+    public void cerrar();
+    public void editar();
+
+}
